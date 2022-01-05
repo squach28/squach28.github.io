91d60627e843928819c4556a38beb66da8cf1fa3
squach28 <squach3443@gmail.com>
2022-01-05 14:27:42 -0800

    dark mode for nav bar implemented

--- script.js
+++ script.js
@@ -3,15 +3,27 @@ var darkMode = false;
 function toggleDarkMode() {
     var darkModeIcon = document.getElementById("darkModeIcon");
     var body = document.body;
+    var navBar = document.getElementById("navBar");
     // fas fa-moon = dark mode enabled
     // far fa-moon = dark mode disabled
     if(darkModeIcon.className == "fas fa-moon") {
         darkModeIcon.className = "far fa-moon"; 
         darkModeIcon.style.border = "1px solid black";
         body.classList.remove("darkMode");
+        for(let i = 0; i < navBar.children.length; i++) {
+            var link = navBar.children[i];
+            link.children[0].style.color = "black";
+            console.log(link.children[0]);
+        }
+        
     } else {
         darkModeIcon.className = "fas fa-moon";
         darkModeIcon.style.border = "1px solid white";
         body.classList.toggle("darkMode");
+        for(let i = 0; i < navBar.children.length; i++) {
+            var link = navBar.children[i];
+            link.children[0].style.color = "white";
+            console.log(link.children[0]);
+        }
     }
 }
