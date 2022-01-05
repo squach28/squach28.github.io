f566ea0a9688cb30b3986120afa14da4f5483e8f
squach28 <squach3443@gmail.com>
2022-01-05 15:03:18 -0800

    adjusted title for dark mode

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var darkMode = false;
 function toggleDarkMode() {
     var darkModeIcon = document.getElementById("darkModeIcon");
     var body = document.body;
+    var title = document.getElementById("title");
     var navBar = document.getElementById("navBar");
     // fas fa-moon = dark mode enabled
     // far fa-moon = dark mode disabled
@@ -10,20 +11,20 @@ function toggleDarkMode() {
         darkModeIcon.className = "far fa-moon"; 
         darkModeIcon.style.border = "1px solid black";
         body.classList.remove("darkMode");
+        title.classList.remove("darkModeTitle");
         for(let i = 0; i < navBar.children.length; i++) {
             var link = navBar.children[i];
             link.children[0].style.color = "black";
-            console.log(link.children[0]);
         }
         
     } else {
         darkModeIcon.className = "fas fa-sun";
         darkModeIcon.style.border = "1px solid white";
         body.classList.toggle("darkMode");
+        title.classList.toggle("darkModeTitle");
         for(let i = 0; i < navBar.children.length; i++) {
             var link = navBar.children[i];
             link.children[0].style.color = "white";
-            console.log(link.children[0]);
         }
     }
 }
